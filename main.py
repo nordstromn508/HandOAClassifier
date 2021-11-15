7f45365c74d7250e5445e2e7a0514977cfb42297
Thurein Myint <trmyint97@gmail.com>
2021-11-15 10:50:26 -0500

    added get_label method

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ main.py
 import glob
 import os
 import time
+import pandas as pd
+import numpy as np
 
 
 def get_data(n=None):
@@ -22,13 +24,35 @@ def get_data(n=None):
         images = [glob.glob("data/FingerJoints/" + x[0:7] + "*") for x in os.listdir("data/FingerJoints/")[0:12*n:12]]
     return images, time.time()-start
 
+def get_label(data):
+
+    """
+    method to get the label of the data.
+    :param data: data to get label of.
+    :return: label of the data.
+    """
+
+    file = pd.read_excel(data)
+    file = file.values[:,:12]
+    list = []
+    for i in range(len(file)):
+        for j in in range(len(file[i])):
+            if file[i][j] == 'nan':
+                file[i][j] = 0
+            
+        list.append(file[i][j])
+
+    return np.array(list)
+    
+
 
 def preprocess(data):
     """
     preprocesses our data so that it is more easily understood by our algorithm.
     :return: preprocessed data.
     """
-    pass
+
+    
 
 
 def data_augment(data):
