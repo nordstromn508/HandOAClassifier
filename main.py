19686c97ccee01569819b5decf89df4666194f8b
nordstromn <nordstromn@wit.edu>
2021-11-14 21:15:17 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+"""
+@author: Nicholas Nordstrom
+main.py
+    Main thread of execution
+
+"""
+
+
+def get_data():
+    """
+    method to import data and get it all nice and ready for learning.
+    :return: our dataset.
+    """
+    pass
+
+
+def preprocess(data):
+    """
+    preprocesses our data so that it is more easily understood by our algorithm.
+    :return: preprocessed data.
+    """
+    pass
+
+
+def data_augment(data):
+    """
+    performs data augmentation as needed.
+    :param data: data to augment.
+    :return: augmented form of the data.
+    """
+    pass
+
+
+def create_cnn():
+    """
+    creates our algorithm to learn from our dataset.
+    :return: the model object.
+    """
+    pass
+
+
+def pipeline():
+    """
+    This is our data 'pipeline' it ensures a consistent flow and modulation of the data.
+    :return: None.
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
