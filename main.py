3cdf68ce387e5bacf69b37e1845a88725ea01950
nordstromn <nordstromn@wit.edu>
2021-11-15 11:33:02 -0500

    implemented create_cnn

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ main.py
     Main thread of execution
 
 """
+import cv2
+import numpy as np
+import tensorflow as tf
+from keras import models, layers, datasets
 
 
 def get_data():
@@ -36,7 +40,20 @@ def create_cnn():
     creates our algorithm to learn from our dataset.
     :return: the model object.
     """
-    pass
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(180, 180, 1)))
+    model.add(layers.AveragePooling2D((2, 2)))
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(90, 90, 1)))
+    model.add(layers.AveragePooling2D((2, 2)))
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(45, 45, 1)))
+    model.add(layers.AveragePooling2D((2, 2)))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(2, activation='softmax'))
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    return model
 
 
 def pipeline():
@@ -48,6 +65,18 @@ def pipeline():
 
 
 def main():
+    paths = ["data/FingerJoints/9000099_dip5.png", "data/FingerJoints/9000099_dip4.png", "data/FingerJoints/9000099_dip3.png", "data/FingerJoints/9000099_dip2.png"]
+    X = []
+    for p in paths:
+        X.append(cv2.imread(p)[:, :, 0])
+    X = np.array(X)
+    y = np.ones(X.shape[0])
+
+    print(X.shape)
+    print(y.shape)
+
+    model = create_cnn()
+    model.fit(X, y)
     pass
 
 
